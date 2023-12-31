4d3092c34058498aed88fb749053cd4ff4f18120
Saurabh Singh <saurabhbans87@gmail.com>
2023-12-31 16:24:04 +0530

    sorted linear search

--- Algorithm/LinearSearch.java
+++ Algorithm/LinearSearch.java
@@ -5,10 +5,14 @@
  */
 package Algorithm;
 
+import java.util.Arrays;
+
 public class LinearSearch {
     public static void main(String[] args) {
         int[] array = {12,13,42,16,76,43,28,27,97,45};
         int key =100;
+        //Time Complexity is O(n) as we need to traverse the complete array in worst case
+        //Space Complexity is O(1) no extra memory is used to allocate the array.
         int index = linearSearch(array, key);
         if(index == -1){
             System.out.println("Key not found");
@@ -16,6 +20,21 @@ public class LinearSearch {
         else{
             System.out.println("Key found at index " +index);
         }
+        sortedLinearSearch(array, key);
+
+    }
+
+    private static int sortedLinearSearch(int[] array, int key) {
+        Arrays.sort(array);
+        for (int i=0; i < array.length-1; i++){
+            if(array[i] == key){
+                return i;
+            }
+            if(array[i] > key){
+                return -1;
+            }
+        }
+        return -1;
     }
 
     private static int linearSearch(int[] array, int key) {
