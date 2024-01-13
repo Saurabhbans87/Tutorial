1bee2121b66b4b2bf09e9ff54a3d2333c6d23b40
Saurabh Singh <saurabhbans87@gmail.com>
2024-01-13 17:51:26 +0530

    next greater nmebr

---
+++ stack/NextGreaterElement.java
@@ -0,0 +1,32 @@
+/**
+ * Create by saurabh
+ * Date: 13/01/24
+ * Project Name: Tutorial
+ */
+package stack;
+
+import java.util.Stack;
+
+public class NextGreaterElement {
+    public static void main(String[] args) {
+        int[] array = {5,6,3,12,10};
+        int[] nextGreatElement = nextGreaterElement(array);
+        for (int i=0;i<array.length;i++){
+            System.out.print(nextGreatElement[i]);
+        }
+    }
+
+    private static int[] nextGreaterElement(int[] array) {
+        int[] result = new int[array.length];
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i< array.length;i++){
+            while(!stack.isEmpty() && array[i] > stack.peek()){
+                int index = stack.pop();
+                //result[index] = array[i];
+                result[i] =result[index];
+            }
+            stack.push(array[i]);
+        }
+        return result;
+    }
+}
