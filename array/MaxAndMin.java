f69b95e244db5b8a7ee4179ff928e17e1cec4b69
Saurabh Singh <saurabhbans87@gmail.com>
2024-01-26 23:12:05 +0530

    seperate zero and one

--- array/MaxAndMin.java
+++ array/MaxAndMin.java
@@ -5,22 +5,25 @@
  */
 package array;
 
+import java.util.Arrays;
+
 public class MaxAndMin {
     public static void main(String[] args) {
         int[] array = {3,5,7,8,9,12};
         maxAndMIn(array);
+        System.out.println(Arrays.toString(array));
     }
 
     private static void maxAndMIn(int[] array) {
-        int max =0;
+        int max = array.length-1;
         int min=0;
         for(int i=0;i< array.length;i++){
-            if(array[i] > max){
-                int temp = array[max];
-                array[min] = array[max];
+            if(array[i] < max){
+                int temp = array[i];
+                array[i] = array[max];
                 array[max] = temp;
-
             }
+            max--;
         }
     }
 }
