b339b7ceae01fa85ae4e157ba2e0f8e5861c9000
Saurabh Singh <saurabhbans87@gmail.com>
2024-01-29 20:02:32 +0530

    seperate zero and one

---
+++ array/MissingNumber.java
@@ -0,0 +1,38 @@
+/**
+ * Create by saurabh
+ * Date: 28/01/24
+ * Project Name: Tutorial
+ */
+package array;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,6,7,8,9};
+        //missingNumber(array);
+        int[] array1 = {12,10,14,8,9,13,15,25,22,25};
+        missingNumberTwoPointer(array1);
+    }
+
+    private static void missingNumberTwoPointer(int[] array) {
+        int first =0;
+        int last =array.length-1;
+        Arrays.sort(array);
+        while(first <=last){
+            if(array[first] ==array[last] ){
+                System.out.println("Repeating element is " +array[first]);
+            }
+            last--;
+        }
+    }
+
+    private static void missingNumber(int[] array) {
+        for(int i=0;i< array.length;i++){
+            if(array[i] != i+1){
+                System.out.println("Missing numebr is " +array[i]);
+                break;
+            }
+        }
+    }
+}
