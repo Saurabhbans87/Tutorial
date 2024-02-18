a3f8c019a5513c75ae13996d00b28a21df9d878f
Saurabh Singh <saurabhbans87@gmail.com>
2024-02-18 22:59:37 +0530

    doubly linkedlsit

--- linkedList/doublyLinkedList/DoublyLinkedListAdd.java
+++ linkedList/doublyLinkedList/DoublyLinkedListAdd.java
@@ -20,21 +20,43 @@ public class DoublyLinkedListAdd {
         Node newNode = new Node(data);
         if(head == null){
             head = newNode;
+            tail = newNode;
         }else{
             newNode.next = head;
             head.prev = newNode;
+            head = newNode;
         }
-        head = newNode;
     }
     public void inseartAtEnd(int data){
         Node newNode = new Node(data);
-        if(tail == null){
+        if(head == null){
+            head = newNode;
             tail = newNode;
-        }else{
-            newNode.prev = tail;
-            tail.next = newNode;
         }
+        Node current = head;
+        while (current.next != null){
+            current = current.next;
+        }
+        tail.next = newNode;
+        newNode.prev = tail;
         tail = newNode;
+    }
+
+    public void forwardLinkedListPrint(){
+        Node current = head;
+        while(current != null){
+            System.out.print(current.data +"->");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+    public void backwardLinkedListPrint(){
+        Node last = tail;
+        while (last != null){
+            System.out.print(last.data + "->");
+            last = last.prev;
+        }
+        System.out.println("null");
     }
 
     public static void main(String[] args) {
@@ -42,9 +64,13 @@ public class DoublyLinkedListAdd {
        doublyLinkedListAdd.inseartAtBeginning(5);
        doublyLinkedListAdd.inseartAtBeginning(10);
        doublyLinkedListAdd.inseartAtBeginning(15);
+       doublyLinkedListAdd.forwardLinkedListPrint();
+       doublyLinkedListAdd.backwardLinkedListPrint();
        doublyLinkedListAdd.inseartAtEnd(20);
        doublyLinkedListAdd.inseartAtEnd(25);
        doublyLinkedListAdd.inseartAtEnd(30);
+       doublyLinkedListAdd.forwardLinkedListPrint();
+       doublyLinkedListAdd.backwardLinkedListPrint();
 
     }
 }
