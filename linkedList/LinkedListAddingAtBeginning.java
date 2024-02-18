e4611effd3a706c9aa273fdb99550b8444e0e657
Saurabh Singh <saurabhbans87@gmail.com>
2024-02-18 22:59:56 +0530

    doubly linkedlsit

--- linkedList/LinkedListAddingAtBeginning.java
+++ linkedList/LinkedListAddingAtBeginning.java
@@ -5,6 +5,8 @@
  */
 package linkedList;
 
+import java.util.Stack;
+
 public class LinkedListAddingAtBeginning {
     static class Node{
         int data;
@@ -27,12 +29,40 @@ public class LinkedListAddingAtBeginning {
             head = newNode;
         }
     }
+    public Node reverseLinkedList(){
+        Node current = head;
+        Node next =  null;
+        Node previous = null;
+        while(current != null){
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+    public void reverseLinkedListUsingStack(){
+        Stack<Node> nodeStack = new Stack<>();
+        Node current = head;
+        while(current != null){
+            nodeStack.push(current);
+            current = current.next;
+        }
+        head = current;
+        while(!nodeStack.isEmpty()){
+            current.next = nodeStack.peek();
+            System.out.println(nodeStack.pop());
+            current = current.next;
+        }
+        current.next = null;
+    }
     public void dispalyLinkedList(){
         Node current = head;
         while(current!=null){
-            System.out.print(current.data+" ");
+            System.out.print(current.data+"->");
             current=current.next;
         }
+        System.out.println("null");
     }
 
     public static void main(String[] args) {
@@ -41,5 +71,9 @@ public class LinkedListAddingAtBeginning {
         linkedListAddingAtBeginning.addingAtBeginning(8);
         linkedListAddingAtBeginning.addingAtBeginning(12);
         linkedListAddingAtBeginning.dispalyLinkedList();
+        Node newHead = linkedListAddingAtBeginning.reverseLinkedList();
+        linkedListAddingAtBeginning.head = newHead;
+        linkedListAddingAtBeginning.dispalyLinkedList();
+        linkedListAddingAtBeginning.reverseLinkedListUsingStack();
     }
 }
